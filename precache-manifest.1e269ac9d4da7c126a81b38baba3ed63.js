0a3ad7afc4e382e1d28e458308ff6a1d943d991b
Kevin C <Wogified@gmail.com>
2019-10-14 20:59:46 -0400

    Updates

---
+++ precache-manifest.1e269ac9d4da7c126a81b38baba3ed63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9c42bf81e41f1525283",
+    "url": "/seasons_hooks/static/js/runtime~main.0e9078d5.js"
+  },
+  {
+    "revision": "9ec9386f2c4dceecb7ca",
+    "url": "/seasons_hooks/static/js/main.840d53b1.chunk.js"
+  },
+  {
+    "revision": "fb18a104f0c67a232d13",
+    "url": "/seasons_hooks/static/js/2.d00d769e.chunk.js"
+  },
+  {
+    "revision": "9ec9386f2c4dceecb7ca",
+    "url": "/seasons_hooks/static/css/main.3daabfff.chunk.css"
+  },
+  {
+    "revision": "faf4add9b48c1f99d5c61225c852650e",
+    "url": "/seasons_hooks/index.html"
+  }
+];
